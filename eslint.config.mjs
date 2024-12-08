3274aa32d4a1663993724a749e986d7389f34a64
Mohammad Forhad <mohammadforhad176@gmail.com>
2024-12-08 20:17:38 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["node_modules", "dist"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+];
